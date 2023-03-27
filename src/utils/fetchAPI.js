88e5041caaacc9c009fd35e0a7123b27f1c3c098
Nosha <Nosha@DESKTOP-HCLS1DA>
2023-03-27 13:10:14 +0330

    init

---
+++ src/utils/fetchAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "a832a0c898mshaf71f1dee1ceb1cp1659f6jsn55035511f3b0",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
